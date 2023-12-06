89b317748f393a5aabe0983bdfc8ead425db5f88
Juho Romppainen <juho.romppainen@tuni.fi>
2023-12-06 10:30:18 +0200

    build_step modified 2

--- puhelinluettelo_frontend/puhelinluettelo_backend/build_step.sh
+++ puhelinluettelo_frontend/puhelinluettelo_backend/build_step.sh
@@ -5,4 +5,4 @@ echo "Build script"
 # add the commands here
 cd .. && npm install
 cd puhelinluettelo_backend && npm install
-cd .. && npm run build && @powershell Copy-Item build -Recurse puhelinluettelo_backend
+rm -rf build && cd .. && npm run build && cp -r build puhelinluettelo_backend
